60b6b32039c98e3e1baa8a7f03af5199bcd961a8
Justin M <manningjustin08@yahoo.com>
2024-10-21 19:23:10 -0400

    adjustments

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-// const adventurer = {
-//   name: "Robin",
-//   health: 10,
-//   inventory: ["sword", "potion", "artifact"],
-//   companion: {
-//     name: "Leo",
-//     type: "Cat",
-//     companion: {
-//       name: "Frank",
-//       type: "FLea",
-//       hat: "fedora",
-//       glasses: "sunglasses",
-//     },
-//   },
+const adventurer = {
+  name: "Robin",
+  health: 10,
+  inventory: ["sword", "potion", "artifact"],
+  companion: {
+    name: "Leo",
+    type: "Cat",
+    companion: {
+      name: "Frank",
+      type: "FLea",
+      hat: "fedora",
+      glasses: "sunglasses",
+    },
+  },
 
-//   roll(mod = 0) {
-//     const result = Math.floor(Math.random() * 20) + 1 + mod;
-//     console.log(`${this.name} rolled a ${result}.`);
-//   },
-// };
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
+  },
+};
 
-// for (let i = 0; i < adventurer.inventory.length; i++) {
-//   console.log(adventurer.inventory[i]);
-// }
+for (let i = 0; i < adventurer.inventory.length; i++) {
+  console.log(adventurer.inventory[i]);
+}
 
-// adventurer.roll();
+adventurer.roll();
 
 class Character {
   constructor(name) {
@@ -38,13 +38,13 @@ class Character {
   }
 }
 
-// const robin = new Character("Robin");
-// robin.inventory = ["sword", "potion", "artifact"];
-// robin.companion = new Character("Leo");
-// robin.companion.type = "Cat";
-// robin.companion.companion = new Character("Frank");
-// robin.companion.companion.type = "Flea";
-// robin.companion.companion.inventory = ["small hat", "sunglasses"];
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
 class Adventurer extends Character {
     constructor (name, role, roles) {
